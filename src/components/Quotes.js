be6c0d0569d200ecb06d8fcae75bb28ae2c660ea
jackline chepngeno <chepngenojackline0@gmail.com>
2022-07-30 10:19:34 +0300

    added content

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,24 +1,27 @@
 import React, {useState} from "react";
 function Quotes({ quote }) {
-    const { id, movie, star, url } = quote
-    const [isLike, setLike] = useState(false)
-    const [countLikes, setCountLikes]=useState(0)
-    function toggleLike() {
-        setLike(isLike => !isLike)
-        setCountLikes(countLikes + 1)
-    }
-    return (
-      <div id="quoteContainer">
-        <div className="card">
-          <div key={id} className="quoteCard">
-            <img height="400px" width='100%' src={url} />
-            <span>
-              <p>Movie: {movie}</p>
-              <p>By: {star}</p>
-            </span>
-                </div>
-                <button onClick={toggleLike}>{isLike ? "Liked" : "Unlike"} {countLikes}</button>   
+  const { id, movie, star, url, favorite } = quote;
+  const [countLikes, setCountLikes] = useState(0);
+
+  function toggleLike() {
+    setCountLikes(countLikes + 1);
+  }
+
+  return (
+    <div id="quoteContainer">
+      <div className="card">
+        <div key={id} className="quoteCard">
+          <img height="400px" width="100%" src={url} alt={movie} />
+          <p>Movie: {movie}</p>
+          <p>By: {star}</p>
         </div>
+        <span>
+          <button onClick={toggleLike} className="like">
+            Likes {countLikes}
+          </button>
+
+        </span>
+      </div>
       </div>
     );
 }
