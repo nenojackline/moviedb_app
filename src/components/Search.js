9a92a4746c492d7c7c539bd798e68311d6d3c5d9
jackline chepngeno <chepngenojackline0@gmail.com>
2022-07-30 10:20:59 +0300

    modify file

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,19 @@
 import React from "react";
 
-function Search(props) {
-    
-    
-    return (
-      <div>
+function Search({ search, onSearch }) {
+  function handleSearch(e) {
+      onSearch(e.target.value);
+  }
+return (
+  <div>
         <input
-          type="search"
-          placeholder="Search..."
-          value={props.value}
-          onChange={(e) => props.setSearch(e.target.value)}
-        ></input>
-      </div>
-    );  
+            className="search"
+      type="search"
+      value={search}
+      placeholder="Search..."
+      onChange={handleSearch}
+    ></input>
+  </div>
+);
 }
 export default Search
